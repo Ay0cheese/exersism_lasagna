4f7cfdbee88f2c66ddbc4026de1242b402c67e13
Ay0cheese <wadegoodman95@gmail.com>
2022-08-02 13:11:17 -0700

    Initial commit. Test for tasks 1, 2 and 3 passing.

---
+++ lasagna.fsx
@@ -0,0 +1,32 @@
+module LuciansLusciousLasagna
+
+// TODO: define the 'expectedMinutesInOven' binding
+// this is a constant value it never changes during the execution of the program. ** Research constants **
+
+
+
+///<summary>
+/// <see cref="Task1">Task 1: </see>
+/// Define the expectedMinutesInOven binding to check how many minutes the lasagna should be in the oven. According to the cooking book, the expected oven time in minutes is 40:
+/// </summary>
+let expectedMinutesInOven = 40//mins
+
+
+// 
+let timePerLayer = 2 //mins
+
+// TODO: define the 'remainingMinutesInOven' function
+// This function takes the elapsed minutes and it substracts that value from the expected minutes
+let remainingMinutesInOven elapsedTimeInOven = expectedMinutesInOven - elapsedTimeInOven
+
+// let result = remainingMinutesInOven 40
+// printfn "(remainingMinutesInOven 40 30) %i" result
+
+// TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes numberOfLayers = numberOfLayers * timePerLayer
+
+
+// TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes a b c = a * b + c
+let results = elapsedTimeInMinutes 3 2 20
+printfn "(elapsedTimeIn Minutes 3 2 20)%i" results
